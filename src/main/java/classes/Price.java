a9f964263bc1a363a96d25a2ec2d5227d61bcf18
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2023-05-11 00:20:43 +0200

    Add getCharge to Price classes

--- src/main/java/classes/Price.java
+++ src/main/java/classes/Price.java
@@ -3,42 +3,41 @@ package classes;
 public abstract class Price {
 	abstract int getPriceCode();
 
-	public double getCharge(int daysRented) {
-		double result = 0;
+	abstract double getCharge(int daysRented); 
+}
 
-		switch (getPriceCode()) {
-			case Movie.REGULAR:
-				result += 2;
-				if (daysRented > 2)
-					result += (daysRented - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				result += daysRented * 3;
-				break;
-			case Movie.CHILDRENS:
-				result += 1.5;
-				if (daysRented > 3)
-					result += (daysRented - 3) * 1.5;
-				break;
-		}
+class ChildrensPrice extends Price {
+	public int getPriceCode() {
+		return Movie.CHILDRENS;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3)
+			result += (daysRented - 3) * 1.5;
 		return result;
 	}
 }
 
-class ChildrensPrice extends Price {
-	int getPriceCode() {
-		return Movie.CHILDRENS;
-	}
-}
-
 class NewReleasePrice extends Price {
-	int getPriceCode() {
+	public int getPriceCode() {
 		return Movie.NEW_RELEASE;
+	}
+
+	public double getCharge(int daysRented) {
+		return daysRented * 3;
 	}
 }
 
 class RegularPrice extends Price {
-	int getPriceCode() {
+	public int getPriceCode() {
 		return Movie.REGULAR;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = 2;
+		if (daysRented > 2)
+			result += (daysRented - 2) * 1.5;
+		return result;
 	}
 }
