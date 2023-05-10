c8c99f8f41c4f7842610e78b1a7b1c36e4042382
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2023-05-10 23:40:20 +0200

    Change amountFor in Customer

--- src/main/java/classes/Customer.java
+++ src/main/java/classes/Customer.java
@@ -19,7 +19,7 @@ public class Customer {
     public String getName (){
         return name;
     };
-    
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
@@ -48,24 +48,8 @@ public class Customer {
     }
 
     private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
+        return aRental.getCharge();
         }
-        return result;
-    }
 
 }
     
