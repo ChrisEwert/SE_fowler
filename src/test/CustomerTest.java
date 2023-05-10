a54b4dfe99cf5da15d1a563baaf9b4f6db2aaa97
Ewert, Christopher - D0306580 <christopher.ewert@dm.de>
2023-05-10 16:20:48 +0200

    Add tests

---
+++ src/test/CustomerTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CustomerTest {
+
+  private Customer customer;
+
+  @BeforeEach
+  public void setUp() {
+    customer = new Customer("Chris");
+  }
+
+  @Test
+  @DisplayName("Get Name")
+  public void getName() {
+    assertEquals("Chris", customer.getName());
+  }
+
+  @Test
+  @DisplayName("Get Statement")
+  public void getStatement() {
+    String statement = "Rental Record for Chris" + "\n";
+    statement += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+    statement += "Amount owed is " + "0.0" + "\n";
+    statement += "You earned " + "0" + " frequent renter points";
+
+    assertEquals(statement, customer.getStatement());
+  }
+}
